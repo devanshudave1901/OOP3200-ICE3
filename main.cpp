f06e13aea320d1a8d4f85e31ab7b93e2899fb9a3
Devanshu Dave <devanshudave1901@gmail.com>
2021-09-30 21:38:10 -0400

    Added more functionality.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
 
 #include <iostream>
+#include <vector>
+
+//#include "Person.h"
+#include "Professor.h"
+#include "Student.h"
 
 int main()
 {
+	
+
+	//Person person = Person("Bob", "Smith", 35.5f);
+	//
+	//person.SaysHello();
+	//std::cout << person.ToString() << std::endl;
+
+	Student student = Student ("Mike", "Johns", 22.0f , "MJ123456");
+	student.SaysHello();
+	student.Studies();
+
+	std::cout << student.ToString() << std::endl;
+
+	Professor professor = Professor("Tom", "Tsilipoulos", 30.0f, "TT23456789");
+	professor.SaysHello();
+	professor.Teaches();
+
+	std::cout << professor.ToString() << std::endl;
+	std::vector<Person*> people;
+
+	people.push_back(new Professor("Alvaro", "Quevedo", 32.0f, "ABC4555"));
+
+	people[0]->SaysHello();
+
+	std::cout << people[0]->ToString() << std::endl;
+
+	people.push_back((new Student("Nick", "Smith", 20.0f, "AB343452454")));
+	people[1]->SaysHello();
+	std::cout << people[1]->ToString() << std::endl;
+
+
+
+
 }
 
